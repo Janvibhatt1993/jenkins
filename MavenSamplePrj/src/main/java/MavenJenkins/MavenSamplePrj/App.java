cf9598fb37e48c82735cd550fe8330e1c609f68e
janvi bhatt <janvibhatt1993@gmail.com>
2022-03-31 14:24:00 +0530

    adding maven project for jenkins

---
+++ MavenSamplePrj/src/main/java/MavenJenkins/MavenSamplePrj/App.java
@@ -0,0 +1,13 @@
+package MavenJenkins.MavenSamplePrj;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
